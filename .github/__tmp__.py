cffaa3422e3e2728563a2299df0c901a15bfe1b4
Beckett <beckett@WiscLab-M1-Max.local>
2025-11-12 10:56:20 -0600

    devops: added documentation building workflow awaiting upload ability

--- .github/__tmp__.py
+++
@@ -1,4 +0,0 @@
-# TODO : CI/CD, publishing app branch -> release branch, ruff, tests, check success of build, security checks, pii-check, etc?
-
-# NOTE: Custom hook for tracking one way dependecies between repositories.
-# NOTE: Github pages? Interactive docs? Tutorials?
